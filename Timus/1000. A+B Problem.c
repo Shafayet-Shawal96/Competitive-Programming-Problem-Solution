60117369e64cc7df6497662e197867ddf18de740
Shafayet-Shawal96 <62823989+Shafayet-Shawal96@users.noreply.github.com>
2020-07-27 21:02:53 +0600

    Create 1000. A+B Problem.c

---
+++ Timus/1000. A+B Problem.c
@@ -0,0 +1,13 @@
+// https://acm.timus.ru/problem.aspx?space=1&num=1000
+
+#include <stdio.h>
+
+int main()
+{
+    int a, b;
+    scanf("%d %d", &a, &b);
+    printf("%d",a+b);
+    return 0;
+}
+
+/* expected the summation of two input number */
