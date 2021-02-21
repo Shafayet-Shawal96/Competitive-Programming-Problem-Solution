b0b85018ee51d9fca67c8383ffa23674a7f31702
Shafayet-Shawal96 <62823989+Shafayet-Shawal96@users.noreply.github.com>
2021-02-21 22:46:54 +0600

    Create CF 1 A. Theatre square.cpp

---
+++ According Websites/CodeForce/CF 1 A. Theatre square.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    long long int n,m,a,x,y;
+    cin>>n>>m>>a;
+    x = n/a;
+    y = m/a;
+    if(n%a>0) x++;
+    if(m%a>0) y++;
+    cout<<x*y<<endl;
+    return 0;
+}
