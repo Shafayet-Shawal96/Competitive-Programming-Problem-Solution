e97e8b530fd80d8db855244b3e5b6237a70e6c5c
Shafayet-Shawal96 <62823989+Shafayet-Shawal96@users.noreply.github.com>
2021-02-25 22:43:45 +0600

    Create LEAP.c

---
+++ According Websites/CodeChef/LEAP.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void) {
+	int n;
+    scanf("%d",&n);
+    if(n%400==0 || (n%100!=0 && n%4==0))
+        printf("TRUE\n");
+    else printf("FALSE\n");
+	return 0;
+}
+
