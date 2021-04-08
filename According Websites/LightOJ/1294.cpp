655edb76048019caf7151db8c0b3f01e930c8f47
Shafayet-Shawal96 <62823989+Shafayet-Shawal96@users.noreply.github.com>
2021-04-08 17:24:38 +0600

    Create 1294.cpp

---
+++ According Websites/LightOJ/1294.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int t,c=1;
+    cin>>t;
+    while(t--){
+        long long n,m;
+        cin>>n>>m;
+        cout<<"Case "<<c<<": "<<n*m/2<<endl;
+        c++;
+    }
+    return 0;
+}
