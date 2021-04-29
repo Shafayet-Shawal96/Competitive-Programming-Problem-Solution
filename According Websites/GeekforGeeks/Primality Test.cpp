31661a8c27daa68faaf927e6184715fcf355aec8
Shafayet-Shawal96 <62823989+Shafayet-Shawal96@users.noreply.github.com>
2021-04-29 23:05:40 +0600

    Create Primality Test.cpp

---
+++ According Websites/GeekforGeeks/Primality Test.cpp
@@ -0,0 +1,44 @@
+// { Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+    public:
+        bool isPrime(int N)
+    {
+        bool ans =1;
+        for(int i=2;i<=sqrt(N);i++){
+            if(N%i==0) ans=0;
+        }
+        return ans;
+    }
+
+};
+
+// { Driver Code Starts.
+
+int main()
+{
+    int T; //testcases
+    cin>>T; //input testcases
+    while(T--) //while testcase have not been exhausted
+    {
+        int N;
+        cin>>N; //input n
+        Solution ob;
+        if(ob.isPrime(N)) 
+            cout<<"Yes";
+        else
+            cout<<"No";
+            
+        cout << endl;
+    }
+    return 0;
+}
+  // } Driver Code Ends
